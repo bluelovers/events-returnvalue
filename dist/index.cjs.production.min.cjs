f172a0276fc44a76926eb10bcf3eba83a0c70069
bluelovers <codelovers@users.sourceforge.net>
2022-10-28 05:53:29 +0800

    .

--- dist/index.cjs.production.min.cjs
+++ dist/index.cjs.production.min.cjs
@@ -1,2 +1,61 @@
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e,t=require("events");exports.EnumInternalEventName=void 0,(e=exports.EnumInternalEventName||(exports.EnumInternalEventName={})).newListener="newListener",e.removeListener="removeListener",e.error="error";class EventEmitter extends t.EventEmitter{on(e,t){return[e].flat().forEach((e=>super.on(e,t))),this}once(e,t){return[e].flat().forEach((e=>super.once(e,t))),this}addListener(e,t){return this.on(e,t)}off(e,t){return[e].flat().forEach((e=>super.off(e,t))),this}removeListener(e,t){return this.off(e,t)}_emit(e,t,r){return _emitArgv(e,t,r)}emit(e,t,...r){return({cacheEvent:t,argv:r}=this._emit(e,t,r)),t.triggered=super.emit(e,t,...r),t}async emitAsync(e,t,...r){({cacheEvent:t,argv:r}=this._emit(e,t,r));let n=!1;for(const i of this.rawListeners(e))await i(t,...r),n=!0;return t.triggered=n,t}}function _createCacheEvent(e,t){var r,n,i,s,a;return null!==(r=t)&&void 0!==r||(t={}),null!==(i=(n=t).triggered)&&void 0!==i||(n.triggered=null),null!==(a=(s=t).name)&&void 0!==a||(s.name=e),"returnValue"in t||(t.returnValue=void 0),t}function _emitArgv(e,t,r){return"newListener"!==e&&"removeListener"!==e||"string"!=typeof t||(r.unshift(t),t=void 0),{event:e,cacheEvent:t=_createCacheEvent(e,t),argv:r}}exports.EventEmitter=EventEmitter,exports._createCacheEvent=_createCacheEvent,exports._emitArgv=_emitArgv,exports.default=EventEmitter;
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: !0
+});
+
+var e, t = require("events");
+
+exports.EnumInternalEventName = void 0, (e = exports.EnumInternalEventName || (exports.EnumInternalEventName = {})).newListener = "newListener", 
+e.removeListener = "removeListener", e.error = "error";
+
+class EventEmitter extends t.EventEmitter {
+  on(e, t) {
+    return [ e ].flat().forEach((e => super.on(e, t))), this;
+  }
+  once(e, t) {
+    return [ e ].flat().forEach((e => super.once(e, t))), this;
+  }
+  addListener(e, t) {
+    return this.on(e, t);
+  }
+  off(e, t) {
+    return [ e ].flat().forEach((e => super.off(e, t))), this;
+  }
+  removeListener(e, t) {
+    return this.off(e, t);
+  }
+  _emit(e, t, r) {
+    return _emitArgv(e, t, r);
+  }
+  emit(e, t, ...r) {
+    return ({cacheEvent: t, argv: r} = this._emit(e, t, r)), t.triggered = super.emit(e, t, ...r), 
+    t;
+  }
+  async emitAsync(e, t, ...r) {
+    ({cacheEvent: t, argv: r} = this._emit(e, t, r));
+    let n = !1;
+    for (const i of this.rawListeners(e)) await i(t, ...r), n = !0;
+    return t.triggered = n, t;
+  }
+}
+
+function _createCacheEvent(e, t) {
+  var r, n, i, s, a;
+  return null !== (r = t) && void 0 !== r || (t = {}), null !== (i = (n = t).triggered) && void 0 !== i || (n.triggered = null), 
+  null !== (a = (s = t).name) && void 0 !== a || (s.name = e), "returnValue" in t || (t.returnValue = void 0), 
+  t;
+}
+
+function _emitArgv(e, t, r) {
+  return "newListener" !== e && "removeListener" !== e || "string" != typeof t || (r.unshift(t), 
+  t = void 0), {
+    event: e,
+    cacheEvent: t = _createCacheEvent(e, t),
+    argv: r
+  };
+}
+
+exports.EventEmitter = EventEmitter, exports._createCacheEvent = _createCacheEvent, 
+exports._emitArgv = _emitArgv, exports.default = EventEmitter;
 //# sourceMappingURL=index.cjs.production.min.cjs.map
